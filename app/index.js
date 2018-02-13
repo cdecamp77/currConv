01c2d7093ad97d716646dd101fc2ad3854c0c1f7
Christopher De Camp <cdecamp77@gmail.com>
2018-02-13 15:47:20 -0800

    File structure, and basic home screen being set-up

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Home from "./screens/Home";
+import EStyleSheet from "react-native-extended-stylesheet";
+
+EStyleSheet.build({
+  $primaryBlue: "#4F607A"
+});
+
+export default () => <Home />;
